81e028fda71be738cd49ea846a4c2d1b68278d39
gustirandi17 <randigusti463@gmail.com>
2025-06-21 13:21:28 +0700

    menggunakan datatypes untuk time_slot

--- entity/schedule.go
+++ entity/schedule.go
@@ -2,14 +2,16 @@ package entity
 
 import (
 	"time"
+
+	"gorm.io/datatypes"
 )
 
 type Schedule struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	ServiceID uint      `gorm:"not null;index" json:"service_id"`    // foreign key ke service
-	Date      string    `gorm:"type:text;not null" json:"date"`      // Format: YYYY-MM-DD
-	TimeSlot  string    `gorm:"type:text;not null" json:"time_slot"` // Format: HH:MM:SS
-	IsBooked  bool      `gorm:"default:false" json:"is_booked"`      // default false
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID        uint      		`gorm:"primaryKey" json:"id"`
+	ServiceID uint      		`gorm:"not null;index" json:"service_id"`    // foreign key ke service
+	Date      string    		`gorm:"type:text;not null" json:"date"`      // Format: YYYY-MM-DD
+	TimeSlot  datatypes.Time    `gorm:"type:time;not null" json:"time_slot"` // Format: HH:MM:SS
+	IsBooked  bool      		`gorm:"default:false" json:"is_booked"`      // default false
+	CreatedAt time.Time 		`gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time 		`gorm:"autoUpdateTime" json:"updated_at"`
 }
